b88e72f151e52c509d75ec5e38410aa22644e88c
Aya Heshmat <ayaheshmatpmd@gmail.com>
2019-09-16 21:02:01 -0400

    load js code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,37 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// hook into the button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// create the function to build the table
+function buildTable(somedata) {
+  let tbody = d3.select("tbody");
+  tbody.html("");
+
+  somedata.forEach(row => {
+    // console.log(row);
+    let tr = tbody.append("tr");
+
+    Object.values(row).forEach(cell => {
+      let c = tr.append("td");
+      c.text(cell);
+    })
+  });
+}
+
+// add function to handle click
+function handleClick() {
+  var date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+  if (date) {
+    filteredData = filteredData.filter( element => element.datetime === date);
+  }
+
+    buildTable(filteredData);
+}
+
+
+// call the build table funciton with the non-filtered data
+buildTable(data);
